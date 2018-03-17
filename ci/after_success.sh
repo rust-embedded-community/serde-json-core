988c88035c26861633717646c785a5db82a99c0c
Jorge Aparicio <jorge@japaric.io>
2018-03-17 14:39:35 +0100

    bors-ng integration

---
+++ ci/after_success.sh
@@ -0,0 +1,24 @@
+set -euxo pipefail
+
+main() {
+    if [ $TARGET = thumbv7m-none-eabi ]; then
+        return
+    fi
+
+    cargo doc --features doc
+
+    mkdir ghp-import
+
+    curl -Ls https://github.com/davisp/ghp-import/archive/master.tar.gz | \
+        tar --strip-components 1 -C ghp-import -xz
+
+    ./ghp-import/ghp_import.py target/doc
+
+    set +x
+    git push -fq https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages && \
+        echo OK
+}
+
+if [ $TRAVIS_BRANCH = master ]; then
+    main
+fi
