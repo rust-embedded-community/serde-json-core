f5f953ab037a3831e2a3e3fea3432d38309d98a3
Jorge Aparicio <jorge@japaric.io>
2018-04-08 23:11:22 +0200

    s/Xargo/Cargo/

--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,12 @@
 set -euxo pipefail
 
 main() {
+    cargo check --target $TARGET
+
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        cargo check --target $TARGET
         cargo test --target $TARGET
         return
     fi
-
-    xargo check --target $TARGET
 }
 
 if [ $TRAVIS_BRANCH != master ]; then
